e8dd7b9973f2359bc19bcd26a7399241e5adc12f
venkatbabukr <venkatbabukr@gmail.com>
2022-04-29 11:17:45 +0530

    Added more functions to StringExt and ArraysExt

--- src/main/java/com/venkat/algos/simple/StringExt.java
+++ src/main/java/com/venkat/algos/simple/StringExt.java
@@ -1,7 +1,12 @@
 package com.venkat.algos.simple;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class StringExt {
 
@@ -20,7 +25,29 @@ public class StringExt {
         return strIsPalindrome;
     }
 
-    public static String longestNonRepeatingSubStr(String s) {
+    public static Character findFirstNonRepeatingChar(String s) {
+        Character firstNonRepeatingChar = null;
+        if(isNonEmpty(s)) {
+            char[] sChars = s.toCharArray();
+            LinkedHashSet<Character> nonRepeatingChars = new LinkedHashSet<>();
+            Set<Character> repeatingChars = new HashSet<>();
+            for (char c : sChars) {
+                if (!repeatingChars.contains(c)) {
+                    if (nonRepeatingChars.remove(c)) {
+                        repeatingChars.add(c);
+                    } else {
+                        nonRepeatingChars.add(c);
+                    }
+                }
+            }
+            if (!nonRepeatingChars.isEmpty()) {
+                firstNonRepeatingChar = nonRepeatingChars.iterator().next();
+            }
+        }
+        return firstNonRepeatingChar;
+    }
+
+    public static String firstLongestNonRepeatingSubStr(String s) {
         String longestSubStr = s;
         if (isNonEmpty(s)) {
             int longestBegin = 0, longestEnd = 0;
@@ -31,8 +58,8 @@ public class StringExt {
                 Integer cPrevIdx = charIdces.get(c);
                 if (cPrevIdx != null && cPrevIdx >= scanBegin) {
                     if (scanEnd - scanBegin - 1 > longestEnd - longestBegin) {
-                    	longestBegin = scanBegin;
-                    	longestEnd = scanEnd;
+                        longestBegin = scanBegin;
+                        longestEnd = scanEnd;
                     }
                     scanBegin = cPrevIdx + 1;
                 } else if (scanEnd - scanBegin + 1 > longestEnd - longestBegin) {
@@ -44,6 +71,41 @@ public class StringExt {
             longestSubStr = s.substring(longestBegin, longestEnd + 1);
         }
         return longestSubStr;
+    }
+
+    private static List<String> checkAndAddToLongestNonRepeatingSubStrs(List<String> longestSubStrs, char[] sChars, int scanBegin, int scanLen, int longestLen) {
+        if (scanLen >= longestLen) {
+            String newLongestSubStr = new String(sChars, scanBegin, scanLen);
+            if (scanLen > longestLen) {
+                longestSubStrs = new ArrayList<>();
+            }
+            longestSubStrs.add(newLongestSubStr);
+        }
+        return longestSubStrs;
+    }
+
+    public static List<String> allLongestNonRepeatingSubStr(String s) {
+        List<String> allLongestNRSS = s == null ? null : new ArrayList<>();
+        if (isNonEmpty(s)) {
+            int longestLen = 0;
+            Map<Character, Integer> charIdces = new HashMap<>();
+            char[] sChars = s.toCharArray();
+            for (int scanBegin = 0, scanEnd = 0, scanLen = 1; scanEnd < sChars.length; scanEnd++, scanLen++) {
+                char c = sChars[scanEnd];
+                Integer cPrevIdx = charIdces.get(c);
+                if (cPrevIdx != null && cPrevIdx >= scanBegin) {
+                    allLongestNRSS = checkAndAddToLongestNonRepeatingSubStrs(allLongestNRSS, sChars, scanBegin, scanLen - 1, longestLen);
+                    longestLen = Math.max(longestLen, scanLen - 1);
+                    scanBegin = cPrevIdx + 1;
+                    scanLen = scanEnd - scanBegin + 1;
+                } else {
+                    allLongestNRSS = checkAndAddToLongestNonRepeatingSubStrs(allLongestNRSS, sChars, scanBegin, scanLen, longestLen);
+                    longestLen = Math.max(longestLen, scanLen);
+                }
+                charIdces.put(c, scanEnd);
+            }
+        }
+        return allLongestNRSS;
     }
 
     public static void main(String[] args) {
@@ -66,9 +128,21 @@ public class StringExt {
                 "ABCDEF"
         };
         for (String s : longestNonRepeatingSubStrTestCases) {
-            System.out.format("Str=%s, longestNonRepeatingSubStr=%s%n", s, longestNonRepeatingSubStr(s));
+            System.out.format("Str=%s, First longestNonRepeatingSubStr=%s, All longestNonRepeatingSubStr=%s%n", s, firstLongestNonRepeatingSubStr(s), allLongestNonRepeatingSubStr(s));
         }
 
+        String[] firstNonRepeatingCharTestCases = new String[] {
+            null,
+            "string",
+            "a",
+            "aaaccd",
+            "aaabccdee",
+            "aabcdbac",
+            "aabbcc"
+        };
+        for (String s : firstNonRepeatingCharTestCases) {
+        	System.out.format("Str=%s, First non repeating char=%c%n", s, findFirstNonRepeatingChar(s));
+        }
     }
 
 }
